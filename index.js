57c5ce35bc317eb488342b06d02dd4dcb2b785e4
James Cole <mail@ofjamescole.com>
2017-02-19 21:32:28 -0500

    Add library to repo :sunrise:

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const getInstagramData = require('./lib/getInstagramData');
+const instagramWall = require('./lib/instagramWall');
+const config = hexo.config;
+const logger = require('hexo-log')({
+  debug: false,
+  silent: false
+});
+
+hexo.extend.filter.register('after_init', function() {
+  return getInstagramData(config, logger);
+});
+
+hexo.extend.helper.register('instagramWall', function() {
+  return instagramWall(logger);
+}, {async: true});
