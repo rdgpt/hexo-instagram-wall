96adf6a1ee1ff52b8436aadc6b3aaea55082f293
James Cole <mail@ofjamescole.com>
2017-02-20 16:56:01 -0500

    Add options for total count and number per row; Use flexbox for styles for now

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 'use strict';
 
+const assign = require('lodash.assign');
 const getInstagramData = require('./lib/getInstagramData');
 const instagramWall = require('./lib/instagramWall');
+const insertAssets = require('./lib/insertAssets');
 const config = hexo.config;
 const logger = require('hexo-log')({
   debug: false,
   silent: false
 });
 
+// set default options
+config.instagramWall = assign({}, {
+  enable: true,
+  waterfall: true,
+  styles: {
+    overlayColor: 'black',
+    gutterSize: '10px'
+  }
+}, config.instagramWall)
+
 hexo.extend.filter.register('after_init', function() {
   return getInstagramData(config, logger);
 });
 
-hexo.extend.helper.register('instagramWall', function(count) {
-  return instagramWall(count, logger);
+hexo.extend.helper.register('instagramWall', function(requestedCount, itemsPerRow, showCaption) {
+  return instagramWall(requestedCount, itemsPerRow, showCaption, config, logger);
 }, {async: true});
+
+hexo.extend.filter.register('after_render:html', function(str, data) {
+  return insertAssets(str, data, config, logger);
+});
